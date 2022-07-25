f4f5ec1e58943b7cfb6b4457d3a8d03e6bf11421
WeeMingQing <e0725595@u.nus.edu>
2022-07-25 21:46:43 +0800

    minor edit to test cases

--- src/Screens/__test__/Loading.test.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import renderer from "react-test-renderer"
-import Loading from "../Loading"
-
-it("test loading page",()=>{
-    const tree = renderer.create(<Loading/>).toJSON();
-    expect(tree).toMatchSnapshot()
-})
--- src/Screens/__test__/flashcard.test.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { FlashCard } from "../flashcard";
-import renderer from 'react-test-renderer'
-
-it("test flashcard page",  () => {
-  const tree = renderer.create(<FlashCard/>).toJSON();
-  expect(tree).toMatchSnapshot()  
-
-})
